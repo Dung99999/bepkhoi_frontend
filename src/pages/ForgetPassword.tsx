39b9eaf86fdaf66e693f162d3cc8be314e8d7326
Spikiwiky <dragon.spiki@gmail.com>
2025-03-07 00:25:43 +0700

    7/3

---
+++ src/pages/ForgetPassword.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import config from "../config/config";
+
+export default function ForgetPassword() {
+    const config1 = config.menuMessage.mess
+    const config2 = config.menuMessage.actor
+
+    return (
+        <>
+        
+        </>
+    );
+}
