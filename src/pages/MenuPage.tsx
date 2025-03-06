704eae23bbf032b6c72d77ad4347c2ed50f3bb6f
Dung99999 <dtxujhe12345@gmail.com>
2025-03-07 00:13:06 +0700

    6/3: Detail productproduct

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -6,7 +6,7 @@ import MenuSidebar from "./../components/MenuPage/MenuSidebar.tsx";
 import "./../style/MenuPage/menuPage.scss";
 
 const allItems = [
-    { id: 1, code: "SP001", name: "Bánh mì", category: "Đồ ăn", price: 20000, cost: 15000, order: 2, imageUrl: ["https://e7.pngegg.com/pngimages/361/627/png-clipart-leaf-logo-green-leaves-green-and-teal-leaf-logo-text-maple-leaf-thumbnail.png", "https://e7.pngegg.com/pngimages/361/627/png-clipart-leaf-logo-green-leaves-green-and-teal-leaf-logo-text-maple-leaf-thumbnail.png"] },
+    { id: 1, code: "SP001", name: "Bánh mì", category: "Đồ ăn", price: 20000, cost: 15000, order: 2, imageUrl: ["https://e7.pngegg.com/pngimages/361/627/png-clipart-leaf-logo-green-leaves-green-and-teal-leaf-logo-text-maple-leaf-thumbnail.png", "https://img.freepik.com/premium-vector/free-vector-beautiful-flying-hummingbird-design-element-banners-posters-leaflets-brochur_1009653-1.jpg"] },
     { id: 2, code: "SP002", name: "Trà sữa", category: "Đồ uống", price: 30000, cost: 20000, stock: 5, order: 1 },
     { id: 3, code: "SP003", name: "Mì gói", category: "Đồ ăn", price: 10000, cost: 8000, stock: 20, order: 5 },
 ];
