11f8ad87fb72d279de802f7e20e0d3a083f242d3
Spikiwiky <dragon.spiki@gmail.com>
2025-03-19 18:04:09 +0700

    Login + VerifyAccount
    Screen design and logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ManageLayout from "./layouts/ManageLayout";
 import MenuPage from "./pages/Manage/MenuPage";
+import LoginPage from "./pages/POS/LoginPage"
+import VerifyAccount from "./pages/POS/VerifyAccount"
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        {/* Trang Login tách riêng bên ngoài */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify" element={<VerifyAccount />} />
+
+        {/* Nhóm trang quản lý */}
         <Route path="/manage" element={<ManageLayout />}>
-          {/* Nếu vào "/" thì tự động chuyển về "/menu" */}
+          {/* Nếu vào "/manage" thì tự động chuyển về "/manage/menu" */}
           <Route index element={<Navigate to="/manage/menu" replace />} />
           {/* Trang menu chính */}
-          <Route path="/manage/menu" element={<MenuPage />} />
-          {/* Có thể thêm các route khác ở đây nếu cần */}
+          <Route path="menu" element={<MenuPage />} />
         </Route>
+
+        {/* Nếu truy cập đường dẫn không hợp lệ, chuyển hướng về login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
