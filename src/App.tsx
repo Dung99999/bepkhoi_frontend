e0342bde199d8783316290fc1e7660cdfeda44cd
Dung99999 <dtxujhe12345@gmail.com>
2025-03-26 23:33:46 +0700

    26/3: Fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ManageLayout from "./layouts/ManageLayout";
-import MenuPage from "./pages/Manage/MenuPage";
-import manangeRoutes from "./Routes/ManageRoutes";
+import LoginPage from "./pages/POS/LoginPage";
+import VerifyAccount from "./pages/POS/VerifyAccount";
+import manageRoutes from "./Routes/ManageRoutes";
 import ForbiddenPage from "./pages/Error/403";
 import NotFoundPage from "./pages/Error/404";
 import ServerErrorPage from "./pages/Error/500";
-import "./pages/Manage/SettingPricePage.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ManageLayout />}>
-          {manangeRoutes.map((route, index) => (
-            <Route 
-              key={index}
-              path={route.path}
-              element={route.element}
-            />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify" element={<VerifyAccount />} />
+
+        <Route path="/manage" element={<ManageLayout />}>
+          <Route index element={<Navigate to="/manage/menu" replace />} />
+          {manageRoutes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
           ))}
         </Route>
 
-        {/* Route for error type */}
-        <Route path="/403" element={<ForbiddenPage/>}/>
-        <Route path="/404" element={<NotFoundPage/>}/>
-        <Route path="/500" element={<ServerErrorPage/>}/>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/403" element={<ForbiddenPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="/500" element={<ServerErrorPage />} />
       </Routes>
     </Router>
   );
--- src/Routes/ManageRoutes.tsx
+++ src/Routes/ManageRoutes.tsx
@@ -3,7 +3,8 @@ import { Navigate } from "react-router-dom";
 import MenuPage from "../pages/Manage/MenuPage";
 import SettingPricePage from "../pages/Manage/SettingPricePage";
 import CustomerManagePage from "../pages/Manage/CustomerManagePage";
-// import more routes
+import CashierManagePage from "../pages/Manage/CashierManagePage";
+import ShipperManagePage from "../pages/Manage/ShipperManagePage";
 
 interface RouteItem {
     path: string;
@@ -11,11 +12,13 @@ interface RouteItem {
     roles: string[];
 }
 
-const manangeRoutes: RouteItem[] = [
-    { path: "/", element: <Navigate to="/menu" replace />, roles: ["admin", "employee"] }, 
-    { path: "menu", element: <MenuPage />, roles: ["admin"] },
-    { path: "settingPrice", element: <SettingPricePage />, roles: ["admin"] },
-    { path: "customer", element: <CustomerManagePage />, roles: ["admin"] }
+const manageRoutes: RouteItem[] = [
+    { path: "/manage", element: <Navigate to="/manage/menu" replace />, roles: ["admin", "employee"] }, 
+    { path: "/manage/menu", element: <MenuPage />, roles: ["admin"] },
+    { path: "/manage/settingPrice", element: <SettingPricePage />, roles: ["admin"] },
+    { path: "/manage/customer", element: <CustomerManagePage />, roles: ["admin"] },
+    { path: "/manage/cashier", element: <CashierManagePage />, roles: ["admin"] },
+    { path: "/manage/shipper", element: <ShipperManagePage />, roles: ["admin"] }
 ];
 
-export default manangeRoutes;
+export default manageRoutes;
